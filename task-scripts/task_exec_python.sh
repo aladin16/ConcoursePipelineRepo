2ce36b0bfa3b61071445091857c8d15aa90932ca
Aladin <riabi.aladin@gmail.com>
2021-01-07 10:14:50 -0500

    add script

--- task-scripts/task_exec_python.sh
+++ task-scripts/task_exec_python.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-python3 app.py
+git clone aladin-repo aladin-repo-updated
+
+cd aladin-repo
+date > bumpme
+
+git config --global user.email "nobody@concourse-ci.org"
+git config --global user.name "Concourse"
+
+git add .
+git commit -m "Bumped date"
